4d68a80d472e174a4a93ba4cfde76a1d193581e3
adb-sh <git@adb.sh>
2021-10-14 18:00:11 +0200

    calculator.py - first working version

--- calculator.py
+++ calculator.py
@@ -1,12 +1,21 @@
 import RPi.GPIO as gpio
 import time
 
+import board, digitalio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+
+RESET_PIN = digitalio.DigitalInOut(board.D4)
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C, reset=RESET_PIN)
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 14)
+
 gpio.setmode(gpio.BCM)
 
-matrix = [["1","2","3", "A"],
-          ["4","5","6", "B"],
-          ["7","8","9", "C"],
-          ["*", "0", "#", "D"]]
+matrix = [[1, 2, 3, "+"],
+          [4, 5, 6, "-"],
+          [7, 8, 9, "x"],
+          ["*", 0, "#", "/"]]
 
 column = [12, 16, 20, 21]
 row = [18, 23, 24, 25]
@@ -15,6 +24,7 @@ for j in range(4):
     gpio.setup(column[j], gpio.OUT)
     gpio.output(column[j], 1)
     gpio.setup(row[j], gpio.IN, pull_up_down=gpio.PUD_UP)
+
 
 def keypad():
     while True:
@@ -29,9 +39,59 @@ def keypad():
             gpio.output(column[j], 1)
     return False
 
+
+def show():
+    image = Image.new("1", (oled.width, oled.height))
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 0), last + operator + current, font=font, fill=255)
+    draw.text((0, 30), "-------------------------", font=font, fill=255)
+    draw.text((0, 46), current, font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+
+def calc(operator, a, b):
+    a, b = float(a), float(b)
+    return str({
+        '+': lambda: a + b,
+        '-': lambda: a - b,
+        'x': lambda: a * b,
+        '/': lambda: a / b,
+    }.get(operator)())[:8]
+
+
+last = ""
+current = ""
+operator = ""
+
+show()
+
 try:
     while True:
-        print(keypad())
+        input = keypad()
+
+        if input == "#":
+            if len(last) and len(current):
+                current = calc(operator, last, current)
+                operator = ""
+                last = ""
+        elif input == "*":
+            last = ""
+            current = ""
+            operator = ""
+        elif type(input) is int:
+            current += str(input)
+        elif type(input) is str:
+            if len(operator) and current == "":
+                operator = input
+            elif len(last) and len(current):
+                last = calc(operator, last, current)
+            else:
+                last = current
+            current = ""
+            operator = input
+
+        show()
         time.sleep(0.2)
 except KeyboardInterrupt:
     gpio.cleanup()
